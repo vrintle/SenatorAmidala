22e889924f34b26f5706617e46cb67cfc780d568
vrintle <vrintle@gmail.com>
2022-03-31 19:14:46 +0530

    ad

---
+++ app/src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDc7wmh1Az0geg-D1v9wMJUPqgqGA5cgdw",
+  authDomain: "drone-food-delivery.firebaseapp.com",
+  projectId: "drone-food-delivery",
+  storageBucket: "drone-food-delivery.appspot.com",
+  messagingSenderId: "188294630080",
+  appId: "1:188294630080:web:dc1f04082f9c2485fbbb25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
