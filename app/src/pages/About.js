42508fe295dec3ce39aa3202753cab4cdece7c94
Rajkumar84478 <95540317+Rajkumar84478@users.noreply.github.com>
2022-04-04 19:38:45 +0530

    fsd

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-    <div>
-      AboutPage!
-    </div>
+    <>
+<div className="card">
+  <img src="vrintle.jpg" alt="John" />
+  <h1>John Doe</h1>
+  <p className="title">CEO and Founder, Example</p>
+  <p>Harvard University</p>
+  <a href="/"><i className="fa fa-dribbble"></i></a>
+  <a href="/"><i className="fa fa-twitter"></i></a>
+  <a href="/"><i className="fa fa-linkedin"></i></a>
+  <a href="/"><i className="fa fa-facebook"></i></a>
+  <p><button>Contact</button></p>
+</div>
+    </>
   );
 }
 
