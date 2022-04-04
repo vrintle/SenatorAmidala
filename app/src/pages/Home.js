8c567aea95806d5bc32828c43a6fcee0808f07d9
vrintle <vrintle@gmail.com>
2022-04-04 14:41:30 +0530

    dwkjhks

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -3,7 +3,6 @@ import { database } from '../firebase-config';
 import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore';
 
 function App() {
-
   const [meals, setMeals] = useState([])
   const [items, setItems] = useState([])
   const mealsRef = collection(database, 'meals')
@@ -95,11 +94,7 @@ function App() {
                         </p>
                       </div>
                       <div style={{ marginLeft:'17px',marginBottom:'10px'}}>
-<<<<<<< HEAD
-                        <button className="btn btn-primary my-2"  style={{ textAlign: 'center',padding: '7px 22px', pointerEvents: 'auto'}} onClick={() => addItem(meal.id)} disabled={!email} >Add to Cart</button>
-=======
-                        <button className="btn btn-primary my-2" style={{ textAlign: 'center',padding: '7px 22px', pointerEvents: 'auto'}} onClick={() => addItem(meal.id)} disabled={false} title={true ? "You should be logged in first to add items." : null}>Add to Cart</button>
->>>>>>> c6872ac0f2bc5ca43274bf9971be7c6a90e3e45e
+                        <button className="btn btn-primary my-2"  style={{ textAlign: 'center',padding: '7px 22px', pointerEvents: 'auto'}} onClick={() => addItem(meal.id)} disabled={false} >Add to Cart</button>
                       </div>
                     </div>
                   </div>
