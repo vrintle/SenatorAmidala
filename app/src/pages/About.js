40aa9e47ba277f546b9be94be72d107b7125da18
Rajkumar84478 <95540317+Rajkumar84478@users.noreply.github.com>
2022-04-05 18:19:53 +0530

    ds

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -1,19 +1,89 @@
 import React from "react";
+// import raj from "./app/public/raj.png";
 
 function App() {
   return (
     <>
-<div className="card">
-  <img src="vrintle.jpg" alt="John" />
-  <h1>John Doe</h1>
-  <p className="title">CEO and Founder, Example</p>
-  <p>Harvard University</p>
-  <a href="/"><i className="fa fa-dribbble"></i></a>
-  <a href="/"><i className="fa fa-twitter"></i></a>
-  <a href="/"><i className="fa fa-linkedin"></i></a>
-  <a href="/"><i className="fa fa-facebook"></i></a>
-  <p><button>Contact</button></p>
-</div>
+    <div className="container">
+      <h1>About US</h1>
+      <p>Here we Deliver Our Foods Items By Drones  </p>
+    </div>
+      <div className="row">
+        <div className="col-6">
+          <div
+            className="card"
+            style={{
+              width: "80%",
+              marginLeft: "50px",
+              marginRight: "15px",
+              marginTop: "30px",
+              marginBottom: "10px",
+              display: "block",
+              height: "90%"
+             
+            }}
+          >
+            <div className="container" >
+              <img
+                src="/images/raj.png"
+                class="card-img-top"
+                alt="..."
+                style={{ height: "280px", width: "250px", marginTop: "5px" }}
+              />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Rajkumar Chauhan</h5>
+              <h3>Web Developer</h3>
+              <h5>Software Engineer</h5>
+              <p className="container">
+                Hello,users My self rajkumar chauhan.I'm 2nd Year Student of
+                delhi Technological University (DTU).
+              </p>
+              <div className="container ">
+                <span>Email: Rajkumarchauhan84478@gmail.com</span>
+                <br />
+                <div>Contact: 8287042907</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-6"  >
+          <div
+            className="card"
+            style={{
+              width: "90%",
+              marginRight: "10px",
+              marginLeft: "5px",
+              marginTop: "30px",
+              marginBottom: "10px",
+              height: "90%"
+            }}
+          >
+            <div className="container" >
+              <img
+                src="/images/vrintle.png"
+                class="card-img-top"
+                alt="..."
+                style={{ height: "280px", width: "250px", marginTop: "5px" }}
+              />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Rahul Verma (vrintle)</h5>
+              <h3>Web Developer</h3>
+              <h5>Software Engineer</h5>
+              <p className="container">
+                Hello,users My self Rahul Verma.I'm 2nd Year Student of delhi
+                Technological University (DTU).
+              </p>
+              <div className="container ">
+                <span>Email: Vrintle@gmail.com</span>
+                <br />
+                <div>Contact: 7266851293</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
